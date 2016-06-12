4b2b585d3d5f937a453527b28008adc733dff4dd
Peter Mitrano <mitranopeter@gmail.com>
2016-06-12 17:44:26 -0400

    more project setup
    it's a node package now?
    using grunt?
    lol what am I doing

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg : grunt.file.readJSON('package.json'),
+    jshint : {
+      files : [
+        'Gruntfile.js',
+        'src/index.js'
+      ]
+    },
+    jsdoc : {
+      doc : {
+        src : [
+          'src/*.js',
+        ],
+        options : {
+          destination : 'doc'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+  grunt.registerTask('dev', ['jshint']);
+  grunt.registerTask('doc', ['jsdoc']);
+  grunt.registerTask('default', ['dev', 'doc']);
+};
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# MyProfessor
+
+#### An Alexa Skill for univeristy students using google calendar
+
+## Development
+
+Use `npm install` to get everything you need.
+Run `grunt`. This will run jshint and generate jsdocs.
